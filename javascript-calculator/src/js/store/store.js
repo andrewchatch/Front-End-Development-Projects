7f7ee1670dbdfb95ef481e6dfb66d0d90436cc72
andrewchatch <71029560+andrewchatch@users.noreply.github.com>
2021-03-15 13:24:14 -0600

    Delete javascript-calculator directory

--- javascript-calculator/src/js/store/store.js
+++
@@ -1,8 +0,0 @@
-import { createStore } from "redux";
-import rootReducer from "../reducers/rootReducer";
-
-const store = createStore(rootReducer);
-
-export const getDisplay = (store) => store.currVal;
-
-export default store;
